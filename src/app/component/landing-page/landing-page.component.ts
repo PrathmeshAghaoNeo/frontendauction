fd3fdf507cd5534212cd935f59a9c5df5fa41730
Prathmesh <prathmeshaghao@gmail.com>
2025-05-28 19:11:31 +0530

    fe

--- src/app/component/landing-page/landing-page.component.ts
+++ src/app/component/landing-page/landing-page.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 
 import { PromotionsComponent } from './promotions/promotions.component';
 import { CategoryCardComponent } from './category-card/category-card.component';
-import { DirectSaleComponent } from './direct-sale/direct-sale.component';
+import {  DirectSaleComponentLP } from './direct-sale/direct-sale.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
     PromotionsComponent,
-    DirectSaleComponent,
+    DirectSaleComponentLP,
     CategoryCardComponent
   ],
   templateUrl: './landing-page.component.html',
