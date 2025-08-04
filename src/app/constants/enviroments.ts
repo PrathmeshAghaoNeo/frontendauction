eb59404fbfdf1d2429307a63f0d8fcb8b4bccb53
Atharva-official-11 <levidemo1234@gmail.com>
2025-08-05 00:29:38 +0530

    added some images and increase storage

--- src/app/constants/enviroments.ts
+++ src/app/constants/enviroments.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:62627/api',
-    imgUrl : 'http://localhost:62627/',
-    baseurl: 'http://localhost:62627/'
+    apiUrl: 'https://localhost:62627/api',
+    imgUrl : 'https://localhost:62627/',
+    baseurl: 'https://localhost:62627/'
   };
   
